f913e5867b6fcd2ccd1b32c75388a1e9a46a69ca
Sangalaswathi <sangalaswath2468@gmail.com>
2025-01-21 11:29:00 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Image from "next/image";
+import Leftpanel from "@/components/shared/Leftpanel";
+export default function Home() {
+  return (
+    <>
+    <div className="center head-text ">This is the home page</div>
+    <Leftpanel/>
+    </>
+  );
+}
